378e457a796916f4ef05d6c97e046f93f7d3b31d
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2021-11-16 18:51:17 -0300

    Add Language

---
+++ source/InnoSetup.ScriptBuilder/Builder/LanguagesBuilder.cs
@@ -0,0 +1,23 @@
+﻿namespace InnoSetup.ScriptBuilder
+{
+    using Model.LanguageSection;
+
+    public class LanguagesBuilder : ParameterSectionBuilderBase<LanguagesBuilder, LanguageEntry>, ILanguageEntryBuilder
+    {
+        public override string SectionName => "Languages";
+
+        public LanguagesBuilder CreateEntry(string name, string messageFile)
+        {
+            CreateEntryInternal();
+            Name(name);
+            MessageFile(messageFile);
+            return this;
+        }
+
+        private LanguagesBuilder Name(string value) => SetPropertyValue(value);
+        private LanguagesBuilder MessageFile(string value) => SetPropertyValue(value);
+        private LanguagesBuilder LicenseFile(string value) => SetPropertyValue(value);
+        private LanguagesBuilder InfoAfterFile(string value) => SetPropertyValue(value);
+        private LanguagesBuilder InfoBeforeFile(string value) => SetPropertyValue(value);
+    }
+}
