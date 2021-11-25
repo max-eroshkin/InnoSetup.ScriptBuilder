08b60479f0456ec35045b440ea8c738c79b0cd72
Max Eroshkin <dealstone@gmail.com>
2021-11-25 09:20:51 +0700

    Added check if all model properties are nullable.

--- source/BuilderTests/BuilderTests.cs
+++ source/BuilderTests/BuilderTests.cs
@@ -1,9 +1,11 @@
 namespace BuilderTests
 {
+    using System;
     using System.Linq;
     using System.Reflection;
     using FluentAssertions;
     using InnoSetup.ScriptBuilder;
+    using InnoSetup.ScriptBuilder.Model;
     using InnoSetup.ScriptBuilder.Model.SetupSection;
     using Xunit;
 
@@ -29,6 +31,21 @@ namespace BuilderTests
         }
 
         [Fact]
+        public void ModelAllPropertiesNullable()
+        {
+            var baseType = typeof(ModelBase);
+            var modelTypes = baseType.Assembly.GetTypes()
+                .Where(x => x.IsAssignableTo(baseType))
+                .ToList();
+            var properties = modelTypes.SelectMany(x => x
+                    .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    .Where(x => x.Name != "Aux"))
+                .ToList();
+
+            properties.Should().OnlyContain(x => IsNullable(x.PropertyType));
+        }
+
+        [Fact]
         public void WriteFile()
         {
             BuilderUtils.Build(
@@ -45,7 +62,8 @@ namespace BuilderTests
 
                     c.Files.CreateEntry(source: @"bin\*", destDir: InnoConstants.App)
                         .Flags(FileFlags.IgnoreVersion | FileFlags.RecurseSubdirs);
-                    c.Files.CreateEntry(source: "SupportTools.addin", destDir: @"{userappdata}\Autodesk\Revit\Addins\2019");
+                    c.Files.CreateEntry(source: "SupportTools.addin",
+                        destDir: @"{userappdata}\Autodesk\Revit\Addins\2019");
                     c.Files.CreateEntry(source: @"bin\Fonts\GraphikLCG-Medium.ttf", destDir: @"{autofonts}")
                         .FontInstall("Graphik LCG")
                         .Flags(FileFlags.OnlyIfDestFileExists | FileFlags.UninsNeverUninstall);
@@ -67,6 +85,13 @@ namespace BuilderTests
             var scriptBuilder = new TestBuilder();
             var result = scriptBuilder.ToString();
             result.Should().NotBeEmpty();
+        }
+
+        private static bool IsNullable(Type type)
+        {
+            if (!type.IsValueType) return true;
+            if (Nullable.GetUnderlyingType(type) != null) return true;
+            return false;
         }
     }
 }
