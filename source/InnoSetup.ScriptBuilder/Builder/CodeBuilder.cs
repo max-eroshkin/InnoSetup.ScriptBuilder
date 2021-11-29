3fa999f2ad46f15d418cc6e57652a443bbda993f
Max Eroshkin <dealstone@gmail.com>
2021-11-29 08:32:53 +0300

    Added Code section.

---
+++ source/InnoSetup.ScriptBuilder/Builder/CodeBuilder.cs
@@ -0,0 +1,26 @@
+﻿namespace InnoSetup.ScriptBuilder
+{
+    using System.IO;
+    using System.Text;
+
+    public class CodeBuilder : IBuilder
+    {
+        private readonly StringBuilder _data = new();
+
+        public string SectionName => "Code";
+        
+        public CodeBuilder CreateEntry(string script)
+        {
+            _data.AppendLine(script);
+            return this;
+        }
+
+        public void Write(TextWriter writer)
+        {
+            if (_data.Length == 0)
+                return;
+            writer.WriteLine($"\r\n[{SectionName}]");
+            writer.Write(_data);
+        }
+    }
+}
